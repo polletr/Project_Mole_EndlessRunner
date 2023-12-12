7e34e2b5d23a14bd5566937d5e5e2a9ade23e1d6
Leonardo Baldicera <l.baldiceradossan277@mybvc.ca>
2023-12-11 19:17:22 -0700

    Background/Camera fix

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,51 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private GameObject cam;
-    [SerializeField] private float speedDifference;
-    [SerializeField] private float boundary;
-    [SerializeField] private Vector2 respawnPos;
-    [SerializeField] private Vector2 startpos;
+    Material mat;
+    float distance;
 
-    private float speed;
+    [Range(0f, 1f)]
+    public float speedMultiplier = 1f;
+
+    private float gameSpeed;
+
     private float length;
 
     // Start is called before the first frame update
     void Start()
     {
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        mat = GetComponent<MeshRenderer>().material;
+
+        length = GetComponent<MeshRenderer>().bounds.size.x;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        speed = GameManager.Instance._gameSpeed - speedDifference;
+        gameSpeed = GameManager.Instance._gameSpeed;
 
-        float dist = Mathf.Repeat(Time.fixedTime * -speed, length);
-        transform.position = new Vector3(startpos.x + dist, transform.position.y, transform.position.z);
+        distance += (Time.fixedDeltaTime * gameSpeed * speedMultiplier) / length;
 
-        // Check if the background has reached the left boundary
-        if (transform.position.x < boundary)
-        {
-            startpos.x += respawnPos.x;
-        }
-        // Check if the background has reached the right boundary
-        else if (transform.position.x > boundary + length)
-        {
-            startpos.x -= respawnPos.x;
-        }
-    }
-
-    // Draw gizmos in the scene view
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(startpos, 0.5f); // Adjust the sphere size as needed
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(respawnPos + startpos, 0.5f); // Adjust the sphere size as needed
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
+        //transform.position = new Vector3(transform.position.x + Time.fixedDeltaTime * -GameManager.Instance._gameSpeed, transform.position.y, transform.position.z);
     }
 }
